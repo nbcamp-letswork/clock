961d2bbf9c80e91c1b312c3fe6bd1e6c3e4ba7ab
LeeSuHyeon <tngus0673@naver.com>
2025-05-21 20:22:29 +0900

    feat: 사운드 선택 버튼 구현

---
+++ Clock/Clock/Presentation/Scene/Timer/CreateTimer/Subview/SoundButton.swift
@@ -0,0 +1,81 @@
+//
+//  SoundButton.swift
+//  Clock
+//
+//  Created by 이수현 on 5/21/25.
+//
+
+import UIKit
+
+final class SoundButton: UIButton {
+    private let textLabel: UILabel = {
+        let label = UILabel()
+        label.text = "타이머 종료 시"
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 16)
+        return label
+    }()
+
+    private let soundLabel: UILabel = {
+        let label = UILabel()
+        label.text = "전파 탐지기"
+        label.textColor = .systemGray
+        label.font = .systemFont(ofSize: 16)
+        return label
+    }()
+
+    private let chevronImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = .init(systemName: "chevron.right")
+        imageView.tintColor = .systemGray
+        return imageView
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setAttributes()
+        setHierarchy()
+        setConstraints()
+    }
+
+    @available(*, unavailable)
+    required init(coder: NSCoder) {
+        fatalError()
+    }
+}
+
+private extension SoundButton {
+    func setAttributes() {
+        self.clipsToBounds = true
+        self.backgroundColor = .gray.withAlphaComponent(0.2)
+    }
+
+    func setHierarchy() {
+        [
+            textLabel,
+            soundLabel,
+            chevronImageView,
+        ].forEach { self.addSubview($0) }
+    }
+
+    func setConstraints() {
+        textLabel.snp.makeConstraints { make in
+            make.verticalEdges.equalToSuperview()
+            make.leading.equalToSuperview().inset(12)
+        }
+
+        soundLabel.snp.makeConstraints { make in
+            make.verticalEdges.equalToSuperview()
+            make.trailing.equalTo(chevronImageView.snp.leading).offset(-4)
+            make.leading.greaterThanOrEqualTo(textLabel.snp.trailing).offset(16)
+        }
+
+        chevronImageView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview().inset(12)
+            make.height.equalTo(12)
+            make.width.equalTo(10)
+        }
+    }
+}
