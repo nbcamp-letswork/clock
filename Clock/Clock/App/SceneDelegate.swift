decc75c0970079cc673628104509708a0b50c865
dbguswls030 <dbguswls030@gmail.com>
2025-05-21 11:25:37 +0900

    feat: TabBar 구현

--- Clock/Clock/App/SceneDelegate.swift
+++ Clock/Clock/App/SceneDelegate.swift
@@ -18,7 +18,7 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         window?.backgroundColor = .background
-        window?.rootViewController = ViewController()
+        window?.rootViewController = BaseTabBarViewController()
         window?.makeKeyAndVisible()
     }
 }
---
+++ Clock/Clock/Presentation/TabBar/Type/TabBarItemType.swift
@@ -0,0 +1,40 @@
+//
+//  TabBarItemType.swift
+//  Clock
+//
+//  Created by 유현진 on 5/21/25.
+//
+
+import Foundation
+
+enum TabBarItemType: CaseIterable {
+    case alarm
+    case stopwatch
+    case timer
+}
+
+extension TabBarItemType {
+    var title: String {
+        switch self {
+        case .alarm: "알림"
+        case .stopwatch: "스톱워치"
+        case .timer: "타이머"
+        }
+    }
+    
+    var imageName: String {
+        switch self {
+        case .alarm: "alarm.fill"
+        case .stopwatch: "stopwatch.fill"
+        case .timer: "timer"
+        }
+    }
+    
+    var index: Int {
+        switch self {
+        case .alarm: 0
+        case .stopwatch: 1
+        case .timer: 2
+        }
+    }
+}
