5d85af6dae8defe875149eaabbef3c4735cfd5d1
LeeSuHyeon <tngus0673@naver.com>
2025-05-21 20:25:30 +0900

    feat: TimerMainView 구현

---
+++ Clock/Clock/Presentation/Scene/Timer/CreateTimer/TimerMainView.swift
@@ -0,0 +1,44 @@
+//
+//  TimerMainView.swift
+//  Clock
+//
+//  Created by 이수현 on 5/21/25.
+//
+
+import UIKit
+
+final class TimerMainView: UIView {
+    let tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.keyboardDismissMode = .onDrag
+        tableView.showsVerticalScrollIndicator = false
+        tableView.register(RecentTimerCell.self, forCellReuseIdentifier: RecentTimerCell.identifier)
+        tableView.register(OngoingTimerCell.self, forCellReuseIdentifier: OngoingTimerCell.identifier)
+        return tableView
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setHierarchy()
+        setConstraints()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
+
+private extension TimerMainView {
+    func setHierarchy() {
+        self.addSubview(tableView)
+    }
+
+    func setConstraints() {
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
+
