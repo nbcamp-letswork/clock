b25d8bbba0f4c644994c535faf289ba2686208e3
LeeSuHyeon <tngus0673@naver.com>
2025-05-21 16:36:43 +0900

    feat: Image로 구성된 버튼 타입 추가

--- Clock/Clock/Presentation/Component/ClockControlButtonType.swift
+++ Clock/Clock/Presentation/Component/ClockControlButtonType.swift
@@ -12,10 +12,12 @@ enum ClockControlButtonType {
     case lap
     case start
     case cancel
+    case startAndStopImage
+    case startImage
 }
 
 extension ClockControlButtonType {
-    var title: String {
+    var title: String? {
         switch self {
         case .restartAndStop:
             return "재개"
@@ -25,10 +27,12 @@ extension ClockControlButtonType {
             return "랩"
         case .cancel:
             return "취소"
+        default:
+            return nil
         }
     }
 
-    var selectedTitle: String {
+    var selectedTitle: String? {
         switch self {
         case .restartAndStop:
             return "일시 정지"
@@ -36,19 +40,23 @@ extension ClockControlButtonType {
             return "중단"
         case .lap, .cancel:
             return title
+        default:
+            return nil
         }
     }
 
-    var titleColor: UIColor {
+    var titleColor: UIColor? {
         switch self {
         case .restartAndStop, .start:
             return .green
         case .lap, .cancel:
             return .white
+        default:
+            return nil
         }
     }
 
-    var selectedTitleColor: UIColor {
+    var selectedTitleColor: UIColor? {
         switch self {
         case .restartAndStop:
             return .orange
@@ -58,19 +66,23 @@ extension ClockControlButtonType {
             return .white
         case .cancel:
             return .white.withAlphaComponent(0.2)
+        default:
+            return nil
         }
     }
 
-    var backgroundColor: UIColor {
+    var backgroundColor: UIColor? {
         switch self {
-        case .restartAndStop, .start:
+        case .restartAndStop, .start, .startImage:
             return .green.withAlphaComponent(0.2)
         case .lap, .cancel:
             return .gray.withAlphaComponent(0.2)
+        default:
+            return nil
         }
     }
 
-    var selectedBackgroundColor: UIColor {
+    var selectedBackgroundColor: UIColor? {
         switch self {
         case .restartAndStop:
             return .orange.withAlphaComponent(0.2)
@@ -78,6 +90,39 @@ extension ClockControlButtonType {
             return .red.withAlphaComponent(0.2)
         case .lap, .cancel:
             return backgroundColor
+        default:
+            return nil
+        }
+    }
+
+    var image: UIImage? {
+        switch self {
+        case .startAndStopImage:
+            return .init(systemName: "play.circle")
+        case .startImage:
+            return .init(systemName: "play.fill")?.withConfiguration(UIImage.SymbolConfiguration(pointSize: 30))
+        default:
+            return nil
+        }
+    }
+
+    var selectedImage: UIImage? {
+        switch self {
+        case .startAndStopImage:
+            return .init(systemName: "pause.circle")
+        default:
+            return nil
+        }
+    }
+
+    var tintColor: UIColor? {
+        switch self {
+        case .startAndStopImage:
+            return .orange
+        case .startImage:
+            return .green
+        default:
+            return nil
         }
     }
 }
