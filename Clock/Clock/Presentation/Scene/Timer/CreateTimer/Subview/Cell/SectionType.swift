207c57ad74c514787ca121ba45bba7163d0f3052
LeeSuHyeon <tngus0673@naver.com>
2025-05-21 20:21:07 +0900

    feat: TableView 섹션 타입 구현

---
+++ Clock/Clock/Presentation/Scene/Timer/CreateTimer/Subview/Cell/SectionType.swift
@@ -0,0 +1,13 @@
+//
+//  SectionType.swift
+//  Clock
+//
+//  Created by 이수현 on 5/21/25.
+//
+
+import Foundation
+
+enum SectionType: Int, CaseIterable {
+    case ongoingTimer
+    case recentTimer
+}
