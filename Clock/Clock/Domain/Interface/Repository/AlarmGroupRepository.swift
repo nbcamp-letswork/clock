aeb6c4c8b5b309e387057ba878e6c40044c0d93f
youseokhwan <youseokhwan15@gmail.com>
2025-05-23 01:51:35 +0900

    feat: AlarmGroupRepository 구현

---
+++ Clock/Clock/Domain/Interface/Repository/AlarmGroupRepository.swift
@@ -0,0 +1,15 @@
+//
+//  AlarmGroupRepository.swift
+//  Clock
+//
+//  Created by youseokhwan on 5/23/25.
+//
+
+import Foundation
+
+protocol AlarmGroupRepository {
+    func fetchAll() async -> Result<[AlarmGroup], Error>
+    func create(_ alarmGroup: AlarmGroup) async -> Result<Void, Error>
+    func update(_ alarmGroup: AlarmGroup) async -> Result<Void, Error>
+    func delete(by id: UUID) async -> Result<Void, Error>
+}
