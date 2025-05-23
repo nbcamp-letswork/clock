02c73a660a1e5329405551ceab9bd38dd7122203
youseokhwan <youseokhwan15@gmail.com>
2025-05-23 11:54:12 +0900

    feat: TimerEntity 스키마 정의

---
+++ Clock/Clock/Data/Model/Timer/TimerEntity.swift
@@ -0,0 +1,17 @@
+//
+//  TimerEntity.swift
+//  Clock
+//
+//  Created by youseokhwan on 5/23/25.
+//
+
+import CoreData
+
+@objc(Timer)
+public class TimerEntity: NSManagedObject {
+    @NSManaged public var id: UUID
+    @NSManaged public var milliseconds: Int16
+    @NSManaged public var currentMilliseonds: Int16
+    @NSManaged public var label: String
+    @NSManaged public var sound: String
+}
