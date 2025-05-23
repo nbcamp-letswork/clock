3845b9e3ecbeae8b02593b9e5a807cf0720ef5e6
youseokhwan <youseokhwan15@gmail.com>
2025-05-23 21:06:42 +0900

    refactor: mapError 구문으로 불필요한 switch 구문 제거

--- Clock/Clock/Data/Repository/DefaultTimerRepository.swift
+++ Clock/Clock/Data/Repository/DefaultTimerRepository.swift
@@ -15,22 +15,15 @@ final class DefaultTimerRepository: TimerRepository {
     }
 
     func fetchAll() async -> Result<(ongoing: [Timer], recent: [Timer]), Error> {
-        let result = await storage.fetchAll { [weak self] entity in
+        await storage.fetchAll { [weak self] entity in
             guard let self else { fatalError() }
             return toDomainTimer(entity)
-        }
-
-        switch result {
-        case .success(let entities):
-            return .success(entities)
-        case .failure(let error):
-            return .failure(error)
-        }
+        }.mapError { $0 as Error }
     }
 
     @discardableResult
     func create(_ timer: Timer) async -> Result<Void, Error> {
-        let result = await storage.insert { context in
+        await storage.insert { context in
             let entity = TimerEntity(context: context)
             entity.id = timer.id
             entity.milliseconds = Int64(timer.milliseconds)
@@ -40,45 +33,25 @@ final class DefaultTimerRepository: TimerRepository {
             entity.isRunning = timer.isRunning
             entity.isActive = true
             return entity
-        }
-
-        switch result {
-        case .success:
-            return .success(())
-        case .failure(let error):
-            return .failure(error)
-        }
+        }.mapError { $0 as Error }
     }
 
     @discardableResult
     func update(_ timer: Timer) async -> Result<Void, Error> {
-        let result = await storage.update(by: timer.id) { context, entity in
+        await storage.update(by: timer.id) { context, entity in
             entity.milliseconds = Int64(timer.milliseconds)
             entity.currentMilliseonds = Int64(timer.currentMilliseconds)
             entity.sound = timer.sound.path
             entity.label = timer.label ?? ""
             entity.isRunning = timer.isRunning
             return entity
-        }
-
-        switch result {
-        case .success:
-            return .success(())
-        case .failure(let error):
-            return .failure(error)
-        }
+        }.mapError { $0 as Error }
     }
 
     @discardableResult
     func delete(by id: UUID) async -> Result<Void, Error> {
-        let result = await storage.delete(by: id)
-
-        switch result {
-        case .success:
-            return .success(())
-        case .failure(let error):
-            return .failure(error)
-        }
+        await storage.delete(by: id)
+            .mapError { $0 as Error }
     }
 }
 
